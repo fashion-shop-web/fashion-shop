39ecfef7d0b6af677e4f95ba7506381c53caf98a
danhpch2802 <91962634+danhpch2802@users.noreply.github.com>
2021-12-24 01:38:54 +0700

    add advanceSearch event to productSearch

--- public/js/productSearch.js
+++ public/js/productSearch.js
@@ -1,7 +1,18 @@
 const searchBar = document.getElementById('serch');
+const advanceSearchButton = document.getElementById('search-button');
 
 searchBar.onkeydown = (e) => {
     if (e.key === 'Enter' && e.target.value !== '') {
         document.location.href = (`/product/search?name=${e.target.value}`);
     }
+}
+
+advanceSearchButton.onclick=function(){
+    var name = document.getElementById("name");
+    var categories = document.getElementById("categories");
+    var brand = document.getElementById("brand");
+    var discount = document.getElementById("discount");
+
+    document.location.href = (`/product/search?name=${name.value}&categories=${categories.value}
+&brand=${brand.value}&discount=${discount.value}`);
 }
